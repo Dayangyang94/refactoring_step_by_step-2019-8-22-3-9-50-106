96883a3081c49b4f14d794fc966b50c103dd35e0
gaoyang <2436512082@qq.com>
2019-08-27 14:12:48 +0800

    重构

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # refactoring_step_by_step
+Driver类
+1.public改为private
+2.定义合法年纪常量
+3.添加get()、set()、getLegalAge()方法
+Police类
+1.获取driver.age通过方法
+2.魔法数字
+
--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -1,9 +1,22 @@
 package com.tws.refactoring;
 
 public class Driver {
-    public int age;
+    private int age;
+    static int LEGAL_AGE=20;
 
     public Driver(int age) {
         this.age = age;
     }
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getLegalAge() {
+		return Driver.LEGAL_AGE;
+	}
+  
 }
